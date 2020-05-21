ec7ef64d8385d22145a618c3b96842ce0f3fece1
thomaslnx <marcos.msilva10@gmail.com>
2020-05-21 11:48:49 -0300

    Tag highlight style complete

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,9 +4,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  max-width: 600px;
+  max-width: 700px;
   margin: auto;
-  border: 1px solid black;
   justify-content: center;
   align-content: flex-start;
   padding: 50px 50px 20px;
